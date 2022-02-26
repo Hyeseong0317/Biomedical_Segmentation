a9ed610df1faeb16845038f2b10e81a39687b895
Sando <comsicomet@gmail.com>
2022-02-26 21:05:37 +0900

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -1,4 +1,4 @@
-# 0.jpeg, 1.jpeg. 2.jpeg, 3.jepg -> .을 기준으로 split후 앞의 숫자를 0으로 인덱싱하여 가져온다
+# C:/intern/ 디렉터리에 존재하는 0.jpeg, 1.jpeg. 2.jpeg, 3.jepg -> .을 기준으로 split후 앞의 숫자를 0으로 인덱싱하여 가져온다
 import os
 for i in sorted(os.listdir("C:/intern/"), key=lambda x: int(x.split(".")[0])):
     print(i)
