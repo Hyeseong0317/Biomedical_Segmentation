35a464840413263e8d48223683d231b46860de5b
Sando <comsicomet@gmail.com>
2022-03-21 00:38:34 +0900

    Create nibabelplot.py

---
+++ nibabelplot.py
@@ -0,0 +1,19 @@
+#%%
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+img = nib.load('/mnt/intern/nnUNet/Aorta_output/AORTA_119.nii.gz')
+
+data = img.get_data()
+
+print(np.unique(data))
+print(data.shape)
+plt.set_cmap('gray')
+for i in range(3):
+    
+    plt.subplot(5, 5,i + 1)
+    plt.imshow(data[:,:,50 +50*i])
+    plt.gcf().set_size_inches(10, 10)
+    
+plt.show()
