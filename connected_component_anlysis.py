26ddd6c2d351d6de126dbe2fcd970a6e2a29dc37
Sando <comsicomet@gmail.com>
2022-03-26 15:38:39 +0900

    Update connected_component_anlysis.py

--- connected_component_anlysis.py
+++ connected_component_anlysis.py
@@ -1,48 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 connectivity_8 = np.array([[1,1,1],
                            [1,0,1],
                            [1,1,1]])
 
-def find labels(labesl, r ,c neighbors):
-  tmp_labels = labels[r-1:r+2, c-1:c+2]*neighbors
-  return np.sort(tmp_labels[np.nonzero(tmp_labels)])
+def find_labels(labels, r, c, neighbors):
+    
+    tmp_labels = labels[r-1:r+2, c-1:c+2]*neighbors
+    
+    return np.sort(tmp_labels[np.nonzero(tmp_labels)])
+
 
 def connected_component_labeling(bin_img, connectivity=connectivity_8):
-  
-  equivalent = []
-  labels = np.zeros_like(bin_img, dtype='int64')
-  next_label = 1
-  
-  # 1st pass
-  for r, row in enumerate(bin_img):
-    for c, pixel in enumerate(row):
+    equivalent = []
+    labels = np.zeros_like(bin_img, dtype='int64')
+    next_label = 1
     
-      if pixel!=0:
-        neighbors = bin_img[r-1:r+2, c-1:c+2]*connectivity
-        num_neighbors = np.count_nonzero(neighbors)
+    # 1st pass
+    for r, row in enumerate(bin_img):
+        for c, pixel in enumerate(row):
         
-        if num_neighbors == 0:
-          label[r,c] = next_label
-          equivalent.append([next_label,next_label])
-          next_label += 1
-        else:
-          L = find_labels(labels, r, c, neighbors)
-          label[r,c] = np.min(L)
-          
-          uni_L = np.unique(L)
-          if len(uni_L)>1:
-            for i, e in enumerate(equivalent):
-              if uni_L[0] in e:
-                equivalent[i].extend(uni_L[1:])
-                equivalent[i] = list(sorted(set(equivalent[i])))
-     
-  # 2nd pass
-  for e in equivalent:
-    for f in reversed(e):
-      labels[labels==f] = e[0]
-  return labels
+            if pixel!=0:
+                neighbors = bin_img[r-1:r+2, c-1:c+2]*connectivity
+                num_neighbors = np.count_nonzero(neighbors)
+                
+                if num_neighbors == 0:
+                    labels[r,c] = next_label
+                    equivalent.append([next_label,next_label])
+                    next_label += 1
+                else:
+                    L = find_labels(labels, r, c, neighbors)
+                    labels[r,c] = np.min(L)
+                
+                    uni_L = np.unique(L)
+                    if len(uni_L)>1:
+                        for i, e in enumerate(equivalent):
+                            if uni_L[0] in e:
+                                equivalent[i].extend(uni_L[1:])
+                                equivalent[i] = list(sorted(set(equivalent[i])))
+            
+# 2nd pass
+    for e in equivalent:
+        for f in reversed(e):
+            labels[labels==f] = e[0]
+    return labels
            
 def threshold_labels(labels, threshold=10000):
   
@@ -58,7 +61,4 @@ def threshold_labels(labels, threshold=10000):
       thr_labels[labels==e] = cnt
   return thr_labels
                 
-                
-    
-                               
-                   
+            
