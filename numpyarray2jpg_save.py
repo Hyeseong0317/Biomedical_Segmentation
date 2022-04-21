d8aa3a416a12ce25abe828dc14c157d5a1dc09b5
Hyeseong Kim <comsicomet@gmail.com>
2022-04-21 18:22:50 +0900

    Update numpyarray2jpg_save.py

--- numpyarray2jpg_save.py
+++ numpyarray2jpg_save.py
@@ -9,3 +9,26 @@ from PIL import Image
 img = Image.open("NASA.jpg")
 imgArray = np.asarray(img)
 print(imgArray.shape)
+
+
+# numpy to jpg
+from PIL import Image
+
+from glob import glob
+data_files = glob('/mnt/intern/3D_vessel/demo_code/data/test/original2D/*.npy')
+examples = []
+for fname in sorted(data_files):  
+    examples += [fname]
+
+for i in range(len((data_files))):
+    img_path = examples[i] # img path
+    A = np.load(img_path)
+    im = Image.fromarray(A)
+    
+    if im.mode != 'RGB':    # 1 channel -> 3 channel
+        im = im.convert('RGB')
+        
+    new_img_path = img_path.replace('original2D', 'original2DforDCGAN') # img path
+    new_img_path = img_path.replace('npy', 'jpg')
+    print(new_img_path)
+    im.save(new_img_path)
