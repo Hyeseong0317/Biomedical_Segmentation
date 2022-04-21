2bf9e67ca4504ed7697b7adc148069e3bb103f55
Hyeseong Kim <comsicomet@gmail.com>
2022-04-21 18:00:27 +0900

    Update numpyarray2jpg_save.py

--- numpyarray2jpg_save.py
+++ numpyarray2jpg_save.py
@@ -1,3 +1,11 @@
+# numpy to jpg
 from PIL import Image
 im = Image.fromarray(A)
-im.save("your_file.jpeg")
+im.save("your_file.jpg")
+
+# jpg to numpy
+import numpy as np
+from PIL import Image
+img = Image.open("NASA.jpg")
+imgArray = np.asarray(img)
+print(imgArray.shape)
