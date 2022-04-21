4f683e98ec0fc8baeaf1cbf776bc2c37dbb0808f
Hyeseong Kim <comsicomet@gmail.com>
2022-04-21 15:48:55 +0900

    Create codeknowledge.py

---
+++ codeknowledge.py
@@ -0,0 +1,9 @@
+# 
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1: # find함수는 'Conv' string을 찾아서 'Conv' 시작위치의 인덱싱을 반환, 없으면 -1을 반환
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
