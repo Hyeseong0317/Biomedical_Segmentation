b584553a91febc8533e29f8c4734c7542c524dc8
Hyeseong Kim <comsicomet@gmail.com>
2022-04-21 15:49:26 +0900

    Update codeknowledge.py

--- codeknowledge.py
+++ codeknowledge.py
@@ -7,3 +7,5 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+
+[::-1] -> 리스트 모슨 원소 역순으로 인덱싱
